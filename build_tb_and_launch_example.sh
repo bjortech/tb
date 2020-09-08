98610b6250b982fa1293708389fd1de903897f71
bjortech <30356972+bjortech@users.noreply.github.com>
2020-09-08 12:47:33 +0200

    Update build_tb_and_launch_example.sh

--- build_tb_and_launch_example.sh
+++ build_tb_and_launch_example.sh
@@ -1,8 +1,12 @@
-cd ~/catkin_ws/src
+sudo apt-get install ros-melodic-oct* ros-melodic-move-base ros-melodic-dynamic-edt-3d ros-melodic-tf2* ros-melodic-hector-sensors-description ros-melodic-pointcloud-to-laserscan ros-melodic-map-server ros-melodic-dwa-local-planner -y
+mkdir -p ~/tb_ws/src
+cd ~/tb_ws/src
+catkin_init_workspace  # initialize your catkin workspace
+echo "source ~/tb_ws/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
 git clone https://github.com/bjortech/tb
-cd ..
+cd ~/tb_ws
 catkin_make
-echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
 roscd tb_simulation
 chmod +x scripts/tb_heightimg2pointcloud_node.py
 chmod +x scripts/tb_simulate_surroundcloud_node.py
