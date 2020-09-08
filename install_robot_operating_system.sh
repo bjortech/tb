26f28db35437a48657001435fa55b8ff4a10602a
bjortech <30356972+bjortech@users.noreply.github.com>
2020-09-08 11:58:43 +0200

    Update and rename install_ROS_build_launch.sh to install_robot_operating_system.sh

--- install_robot_operating_system.sh
+++ install_robot_operating_system.sh
@@ -12,14 +12,3 @@ rosdep update
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
 catkin_init_workspace  # initialize your catkin workspace
-git clone https://github.com/bjortech/tb
-cd ..
-catkin_make
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
-roscd tb_simulation
-chmod +x scripts/tb_heightimg2pointcloud_node.py
-chmod +x scripts/tb_simulate_surroundcloud_node.py
-roscd tb_invoke
-chmod +x tb_invoke_node.py
-source ~/.bashrc
-roslaunch tb_bringup tb_without_premade_octomap.launch
