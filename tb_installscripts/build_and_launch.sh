93e41f1d4429b686197000c8b9494d40aa44d218
tord bjoerlykke <bjortech@gmail.com>
2020-09-22 10:58:14 +0200

    update install script

--- tb_installscripts/build_and_launch.sh
+++ tb_installscripts/build_and_launch.sh
@@ -7,10 +7,5 @@ source ~/.bashrc
 git clone https://github.com/bjortech/tb
 cd ~/tb_ws
 catkin_make
-roscd tb_simulation
-chmod +x scripts/tb_heightimg2pointcloud_node.py
-chmod +x scripts/tb_simulate_surroundcloud_node.py
-roscd tb_invoke
-chmod +x tb_invoke_node.py
 source ~/.bashrc
 roslaunch tb_bringup tb_premade_octomap.launch
